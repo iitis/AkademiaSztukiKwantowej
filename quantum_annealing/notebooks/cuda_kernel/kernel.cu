4b5bf1a9b3424359be2f4894abf1c5af538f00b7
tomsmierz <smierzchalski.tomek@gmail.com>
2025-02-16 23:09:50 +0100

    zmiana @cuda.jit na kernel w c++

---
+++ quantum_annealing/notebooks/cuda_kernel/kernel.cu
@@ -0,0 +1,48 @@
+#define N 216  // ilość spinow
+#define M 2^10  // ilość trajektorii
+
+// Pełen pegaz
+// #define N 5400  // ilość spinow
+// #define M 1024 
+
+extern "C"{ // wymagane , ponieważ c++ naturalnie dekoruje (name mangling) nazwy funkcji, przez co trudno je potem linkować
+
+__global__ void parrarel_annealing_step(float A[N][M], float h[N], float x[N][M], float momentum[N][M], // macierze wejściowe
+                                        float lambda_t, float step_size,  // stałe
+                                        float momentum_new[N][M], float x_new[N][M], float state_new[N][M]) { // macierze wyjsciowe
+
+    float beta = 1.0F - step_size;
+
+    int ti = threadIdx.x;  // pojedyńczy element w kolumnie
+    int col = blockIdx.x;  // każdy blok zajmuje się jedną kolumną (trajektorią)
+    int k = blockIdx.y;  // ewentualne dodatkowe bloki na kolumne
+    int blockSize = blockDim.x; // rozmiar bloku
+
+    // gdzie "globalnie" jesteśmy w macierzy
+    int global_row = ti + k * blockSize;
+
+    if (global_row < N){
+        float x_ij = x[global_row][col];
+
+        float gradient = -1.0F * A[global_row][col] - h[global_row] + x_ij * lambda_t;
+        float momentum_value = momentum[global_row][col] * beta - step_size * gradient;  // momentum 
+
+        float momentum_clipped = max(-1.0F, min(1.0F, momentum_value));
+        float x_new_clipped =  max(-1.0F, min(1.0F, x_ij + momentum_clipped));
+
+        momentum_new[global_row][col] = momentum_clipped;  // nowe momentum
+        x_new[global_row][col] = x_new_clipped;  // nowy x
+        // Rzutowanie stanu
+        if (x_new_clipped > 0){
+            state_new[global_row][col] = 1.0F;
+        } else if (x_new_clipped < 0){
+            state_new[global_row][col] = -1.0F;
+        } else {
+            state_new[global_row][col] = 0.0F;
+        }
+
+    }    
+}
+
+
+}
