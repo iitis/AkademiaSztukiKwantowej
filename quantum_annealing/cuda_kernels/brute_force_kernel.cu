d28d4445ac427749a1ca76d3b4eeccbbbcdb13c7
tomsmierz <smierzchalski.tomek@gmail.com>
2025-02-27 19:20:14 +0100

    WIP

--- quantum_annealing/cuda_kernels/brute_force_kernel.cu
+++ quantum_annealing/cuda_kernels/brute_force_kernel.cu
@@ -1,11 +1,11 @@
 #include <thrust/sort.h>
 #include <thrust/functional.h>
 
-#define N 32 // ilość spinów
+#define N 20 // ilość spinów
 
 extern "C" {
 
-__global__ void cumpute_energies(float Q[N][N], int sweep_size, float* energies, long* states, int offset){
+__global__ void compute_energies(float Q[N][N], int sweep_size_exponent, float* energies, long* states, int offset){
 
 
 	int ti = threadIdx.x;
@@ -16,11 +16,13 @@ __global__ void cumpute_energies(float Q[N][N], int sweep_size, float* energies,
 	int global_idx = ti + num_threads * block_idx;
 	int total_threads = num_threads * grid_size;
 
+	int chunk_size = (1 << sweep_size_exponent);  // równoważne 2^sweep_size_exponent  
+
 	__shared__ float sQ[N][N];
 
 	// ładujemy wpółdzieloną pamięć
 
-	for (int idx = ti; idx <= N*N; idx = idx + num_threads){
+	for (int idx = ti; idx < N*N; idx = idx + num_threads){
 		int i = idx / N;
 		int j = idx % N;
 		sQ[i][j] = Q[i][j];
@@ -28,25 +30,25 @@ __global__ void cumpute_energies(float Q[N][N], int sweep_size, float* energies,
 	}
 	__syncthreads();
 
-	for (long idx = global_idx; idx <= pow(2.0F, float(sweep_size)); idx = idx + total_threads){
-		long state_code = idx + offset;
+	for (long idx = global_idx; idx < chunk_size; idx = idx + total_threads){
+		long state_code = idx + offset * chunk_size;
 		bool binary[N];
 		float en = 0.0F;
 		
-		for (int i = 0; i <= N; i++){
+		for (int i = 0; i < N; i++){
 			binary[i] = (state_code >> i) & 1;
 		}
 
-		// for (int i = 0; i <= N; i++){
-		// 	if (binary[i] == 1){
-		// 		en = en + sQ[i][i];
-		// 		for (int j = i; j <= N; j++){
+		for (int i = 0; i < N; i++){
+			if (binary[i] == 1){
+				en = en + sQ[i][i];
+				for (int j = i + 1; j < 5; j++){
 
-		// 			en = en + sQ[i][j] * binary[j];
+					en = en + sQ[i][j] * binary[j];
 
-		// 		}
-		// 	}
-		// }
+				}
+			}
+		}
 		states[idx] = state_code;
 		energies[idx] = en;
 	}
