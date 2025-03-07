4d6f742a989b37df9786e000845098fa7ade807a
tomsmierz <smierzchalski.tomek@gmail.com>
2025-03-07 18:23:24 +0100

    sbm bugfix

--- classical_and_quantum_annealing/cuda_kernels/sbm_kernel.cu
+++ classical_and_quantum_annealing/cuda_kernels/sbm_kernel.cu
@@ -1,11 +1,9 @@
-#define N 216  // ilość spinow
-#define M 1000  // ilość trajektorii
 
 extern "C"{ // wymagane , ponieważ c++ naturalnie dekoruje (name mangling) nazwy funkcji, przez co trudno je potem linkować
 
-__global__ void update_x_and_wall(float x[N][M], float y[N][M], 
-                         float a_0, float time_step,
-                         float x_new[N][M], float y_new[N][M]){
+__global__ void update_x_and_wall(float* x, float* y, 
+                         float a_0, float time_step, int N,
+                         float* x_new, float* y_new){
     
     int ti = threadIdx.x;  // pojedyńczy element w kolumnie
     int col = blockIdx.x;  // każdy blok zajmuje się jedną kolumną (trajektorią)
@@ -14,27 +12,28 @@ __global__ void update_x_and_wall(float x[N][M], float y[N][M],
 
     // gdzie "globalnie" jesteśmy w macierzy
     int global_row = ti + k * blockSize;
+    int num_cols = gridDim.x;
 
     if (global_row < N){
-        float y_ij = y[global_row][col];
-        float temp1 = x[global_row][col] + a_0 * y_ij * time_step;  // krok x
+        float y_ij = y[global_row * num_cols + col];
+        float temp1 = x[global_row * num_cols + col] + a_0 * y_ij * time_step;  // krok x
 
 
         // "sciana"
         if (abs(temp1) > 1){
-            x_new[global_row][col] = max(-1.0F, min(1.0F, temp1));
-            y_new[global_row][col] = 0.0F;
+            x_new[global_row * num_cols + col] = max(-1.0F, min(1.0F, temp1));
+            y_new[global_row * num_cols + col] = 0.0F;
         } else{
-            x_new[global_row][col] = temp1;
-            y_new[global_row][col] = y_ij;
+            x_new[global_row * num_cols + col] = temp1;
+            y_new[global_row * num_cols + col] = y_ij;
         }
     
     }
 }   
 
-__global__ void update_y(float x[N][M], float y[N][M], float A[N][M], 
-                         float a_0, float a_t, float c_0, float time_step, 
-                         float y_new[N][M]){
+__global__ void update_y(float* x, float* y, float* A, 
+                         float a_0, float a_t, float c_0, float time_step, int N,
+                         float* y_new){
     int ti = threadIdx.x;  // pojedyńczy element w kolumnie
     int col = blockIdx.x;  // każdy blok zajmuje się jedną kolumną (trajektorią)
     int k = blockIdx.y;  // ewentualne dodatkowe bloki na kolumne
@@ -42,15 +41,16 @@ __global__ void update_y(float x[N][M], float y[N][M], float A[N][M],
 
     // gdzie "globalnie" jesteśmy w macierzy
     int global_row = ti + k * blockSize;
+    int num_cols = gridDim.x;
 
     float a = -1.0F * (a_0 - a_t);
 
     if (global_row < N){
-        float y_ij = y[global_row][col];
-        float x_ij = x[global_row][col];
-        float a_ij = A[global_row][col];
+        float y_ij = y[global_row * num_cols + col];
+        float x_ij = x[global_row * num_cols + col];
+        float a_ij = A[global_row * num_cols + col];
         float temp = y_ij + (a * x_ij + c_0 * a_ij) * time_step;
-        y_new[global_row][col] = temp;
+        y_new[global_row * num_cols + col] = temp;
     }
 }
 
