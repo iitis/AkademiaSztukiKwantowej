d9491b306e956f38f9ef701e198ba153dba99d1d
tomsmierz <smierzchalski.tomek@gmail.com>
2025-03-18 13:33:32 +0100

    nicer bruteforce picture

--- classical_and_quantum_annealing/benchmarks.py
+++ classical_and_quantum_annealing/benchmarks.py
@@ -86,10 +86,10 @@ def sort_by_key(energies, states, num_states):
     return low_energies, low_states
 
 
-def brute_force_gpu(Q,  num_states: int, sweep_size_exponent: int = 10):
+def brute_force_gpu(Q,  num_states: int, sweep_size_exponent: int = 10, threadsperblock: int = 256):
     N, _ = Q.shape
-    if N > 32:
-        raise ValueError("Za wysoka wartość N. Ta implementacja wspiera co najwyżej 32 spiny (32-bitowy int)")
+    if N > 64:
+        raise ValueError("Za wysoka wartość N. Ta implementacja wspiera co najwyżej 64 spiny (64-bitowy integer)")
     sweep_size = 2**sweep_size_exponent
     num_chunks = 2**(N-sweep_size_exponent)
 
@@ -97,7 +97,6 @@ def brute_force_gpu(Q,  num_states: int, sweep_size_exponent: int = 10):
     brute_force_kernel = cp.RawModule(path="cuda_kernels/brute_force_kernel.ptx")
     compute_energies = brute_force_kernel.get_function("compute_energies")
 
-    threadsperblock = 256
     blockspergrid = sweep_size//threadsperblock
     
     final_energies = cp.array([])
@@ -106,16 +105,16 @@ def brute_force_gpu(Q,  num_states: int, sweep_size_exponent: int = 10):
     for i in tqdm(range(num_chunks), desc="wyczerpujące przeszukiwanie"):
 
         energies = cp.empty(sweep_size, dtype=cp.float32)
-        states = cp.empty(sweep_size, dtype=cp.int32)
+        states = cp.empty(sweep_size, dtype=cp.int64)
         compute_energies((blockspergrid,), (threadsperblock,), 
-                         (Q, cp.int32(N), cp.int32(sweep_size_exponent), energies, states, cp.int32(i)))
+                         (Q, cp.int32(N), cp.int32(sweep_size_exponent), energies, states, cp.int64(i)))
 
         low_energies, low_states = select_lowest(energies, states, num_states)
         
         final_energies = cp.concatenate((final_energies, low_energies))
         final_states = cp.concatenate((final_states, low_states))
+        
         if i != 0:
-
             final_energies, final_states = select_lowest(final_energies, final_states, num_states)
 
     return sort_by_key(final_energies, final_states, num_states)
