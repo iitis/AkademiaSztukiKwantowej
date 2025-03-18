f84330f23a9d79a3f51a45a11d5e0fc3bbcc366d
tomsmierz <smierzchalski.tomek@gmail.com>
2025-03-18 12:12:00 +0100

    removed 32-spin limit

--- classical_and_quantum_annealing/cuda_kernels/brute_force_kernel.cu
+++ classical_and_quantum_annealing/cuda_kernels/brute_force_kernel.cu
@@ -1,9 +1,9 @@
-#define N 32
+#define N 64
 
 extern "C" {
 
 
-__global__ void compute_energies(float* Q, int size,  int sweep_size_exponent, float* energies, int* states, int offset){
+__global__ void compute_energies(float* Q, int size,  int sweep_size_exponent, float* energies, long* states, long offset){
 
 
 	int ti = threadIdx.x;
@@ -14,7 +14,7 @@ __global__ void compute_energies(float* Q, int size,  int sweep_size_exponent, f
 	int global_idx = ti + num_threads * block_idx;
 	int total_threads = num_threads * grid_size;
 
-	int chunk_size = (1 << sweep_size_exponent);  // równoważne 2^sweep_size_exponent  
+	long chunk_size = (1 << sweep_size_exponent);  // równoważne 2^sweep_size_exponent  
 
 	__shared__ float sQ[N][N];
 
@@ -28,14 +28,14 @@ __global__ void compute_energies(float* Q, int size,  int sweep_size_exponent, f
 	}
 	__syncthreads();
 
-	for (int idx = global_idx; idx < chunk_size; idx = idx + total_threads){
-		int state_code = idx + offset * chunk_size;
+	for (long idx = global_idx; idx < chunk_size; idx = idx + total_threads){
+		long state_code = idx + offset * chunk_size;
 		float en = 0.0F;
 		
 		for (int i = 0; i < size; i++){
 			bool bit_i = (state_code >> i) & 1;
 			if (bit_i){
-				en = en + bit_i * sQ[i][i];
+				en = en + sQ[i][i];
 				for (int j = i + 1; j < size; j++){
 					bool bit_j = (state_code >> j) & 1;
 					en = en + sQ[i][j] * bit_j;
